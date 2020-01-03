096481dd85d641a20da08d38fda7889ca95fa537
Stef Thoen <stef@baardbaard.nl>
2020-01-03 09:18:51 +0100

    Refactor class components to functional components

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,43 +4,41 @@ import Image from 'gatsby-image'
 
 import { rhythm } from '../utils/typography'
 
-function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author, social } = data.site.siteMetadata
-        return (
-          <div
+const Bio = () => (
+  <StaticQuery
+    query={bioQuery}
+    render={data => {
+      const { author, social } = data.site.siteMetadata
+      return (
+        <div
+          style={{
+            display: `flex`,
+            marginBottom: rhythm(2.5),
+          }}
+        >
+          <Image
+            fixed={data.avatar.childImageSharp.fixed}
+            alt={author}
             style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5),
+              marginRight: rhythm(1 / 2),
+              marginBottom: 0,
+              minWidth: 50,
+              borderRadius: `100%`,
             }}
-          >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-            />
-            <p>
-              Written by <strong>{author}</strong> who lives and works in
-              Morvan, France.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
-            </p>
-          </div>
-        )
-      }}
-    />
-  )
-}
+          />
+          <p>
+            Written by <strong>{author}</strong> who lives and works in Morvan,
+            France.
+            {` `}
+            <a href={`https://twitter.com/${social.twitter}`}>
+              You should follow him on Twitter
+            </a>
+          </p>
+        </div>
+      )
+    }}
+  />
+)
 
 const bioQuery = graphql`
   query BioQuery {
